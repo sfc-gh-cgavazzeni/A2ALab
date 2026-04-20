7525f8a0aa994adf0a3598bbf2c067b676312ca0
Cristian Gavazzeni <cristian.gavazzeni@snowflake.com>
2026-04-20 12:50:52 +0200

    Update 00_setup_account_trial.sql

--- 00_setup_account_trial.sql
+++ 00_setup_account_trial.sql
@@ -10,3 +10,9 @@
 --- 4. Attivare l'account tramite il link ricevuto via email
 --- 5. Accedere a Snowsight
 --- ============================================================
+
+CREATE OR REPLACE API INTEGRATION MY_GIT_API_INTEGRATION
+    API_PROVIDER = GIT_HTTPS_API
+    API_ALLOWED_PREFIXES = ('https://github.com/sfc-gh-cgavazzeni')
+    ENABLED = TRUE;
+
